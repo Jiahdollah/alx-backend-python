22f27d4354cf83280d0c311e969b8ddf3d7b0435
peter njihia <peternjihia2001@gmail.com>
2023-10-05 21:36:29 +0000

    basic annotaions-concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""write annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string
+"""
+
 
 def concat(str1: str, str2: str) -> str:
-    """concatenate two strings and return the results"""
+    """Concatenate this two strings"""
     return str1 + str2
-
